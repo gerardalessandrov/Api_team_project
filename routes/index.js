e7e36b4a6b354c9d19d90826f66d8e8d9a00efd8
gerardalessandrov <gerardvigorodrigues@gmail.com>
2025-04-17 16:34:37 -0500

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,14 @@
 const passport = require("passport");
 const router = require("express").Router();
-const contacts = require("./contacts");
-const products = require("./products");
-
-// Si quieres tener una ruta GET explícita para ver que funcionan
-router.get("/contacts", contacts);  // <-- ❌ Esto dará error, porque contacts es un router, no un handler
-router.get("/products", products);  // <-- ❌ Mismo problema
 router.use("/", require("./swagger"));
 router.get("/", (req, res) => {
   res.send("Logged out");
 });
-router.get("/tasks", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Logged out");
 });
-// router.use("/contacts", require("./contacts"));
-// router.use("/products", require("./products"));
+router.use("/contacts", require("./contacts"));
+router.use("/products", require("./products"));
 router.get("/login", passport.authenticate("github"), (req, res) => {});
 router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
